a2846ab7c65abe47e6f847d183ca58c32e0d0de9
katwal-dipak <katwal.dipak@hotmail.com>
2018-07-29 16:21:23 +0530

    Minor Fix

--- src/appEntry/App.js
+++ src/appEntry/App.js
@@ -33,7 +33,9 @@ const StackNavigation = StackNavigator({
             },
             headerStyle: {
                 backgroundColor: '#2a2a2a',
-                elevation: 0 //Only for Android
+                elevation: 0, //Only for Android
+                borderBottomColor: 'transparent', //For ios
+                borderBottomWidth: 0
             },
             headerTintColor: '#FFFFFF',
             title: "Product Description",
